170688b3fa0121947eb81093750410846b3e0ad1
steph-d989 <steb_d@hotmail.com>
2024-05-13 16:34:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 /*Ejercicio 1:
 Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase*/
 
-function contarLetra (frase, letra){
+function contarLetra(frase, letra) {
+    let contador = 0;
+    for (let i = 0; i < frase.length; i++) {
 
-    for(let i=0; i<frase.length; i++){
-        let contador=0;
-        if(frase[i]===letra){
+        if (frase[i] === letra) {
             contador++
         }
     }
     return contador;
 }
-
-console.log(contarLetra("Esta es la frase en la que quiero contar las aes", "a"));
-
+console.log(contarLetra("Que es esto", "e"))
 /*Ejercicio 2:
 Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números*/
 let numeroUsuario = prompt("Ingresa un numero: ");
-let array =[];
-for(let i = numeroUsuario; i<=numeroUsuario+50; i++){
-    if(i%2!==0){
-        array.push(i)
+function printImpares(numeroUsuario) {
+    let array = [];
+    for (let i = numeroUsuario; i <= numeroUsuario + 50; i++) {
+        if (i % 2 !== 0) {
+            array.push(i)
+        }
     }
+    return array;
 }
+console.log(printImpares(50));
